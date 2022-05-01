cddabd324e29266346e91daae00b9f34a9f076e2
Nik1am <61971270+Nik1am@users.noreply.github.com>
2022-05-01 19:32:18 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ print(""" ___ _____ ________  _________  ___  ______  ___ _____ ___________ ___
 | | | \__/\ \_/ / |  | || |   | | | || |\ \| | | || | \ \_/ / |\ \  | |
 | |_ \____/\___/\_|  |_/\_|   \_| |_/\_| \_\_| |_/\_/  \___/\_| \_|_| |
 |___|                                                             |___|
-                                                                       """)
+                                                                       
+(type h for help)""")
 def term():
     ti = input('$ ')
     if ti == 'h':
@@ -28,6 +29,8 @@ def term():
         print_compared_dirs(listed_dir_old,listed_dir_new)
     elif ti == 'a':
         init_atp()
+    elif ti == 'f':
+        compare_file()
     
     term()
 
@@ -57,6 +60,34 @@ def init_atp():
     dir_new = in2+atp
     listed_dir_new = os.listdir(dir_new)
 
+def compare_file():
+    offset_old = 0
+    offset_new = 0
+    name = input('File name: ')
+    f1 = open(dir_old+name,'r').readlines()
+    f2 = open(dir_new+name,'r').readlines()
+    for string in range(0,max(len(f1),len(f2))):
+        strnum = f'[{string:03d}]'
+        old_elem = f1[string-offset_old]
+        new_elem = f2[string-offset_new]
+        
+        try:
+            file1_str = "{:>52}".format(old_elem)
+        except:
+            file1_str = ' '*STRING_LENGTH
+        try:
+            file2_str = "{:>52}".format(new_elem)
+        except:
+            file2_str = ' '*STRING_LENGTH
+            
+        if new_elem not in f1:
+            print(colorama.Back.GREEN,strnum,' '*STRING_LENGTH,file2_str,colorama.Back.BLACK)
+            offset_old = offset_old + 1
+        elif old_elem not in f2:
+            print(colorama.Back.RED , strnum,file1_str,' '*STRING_LENGTH,colorama.Back.BLACK)
+            offset_new = offset_new + 1
+        elif old_elem == new_elem:
+            print(colorama.Back.BLACK ,strnum,file1_str,file2_str)
 
 def print_compared_dirs(listed_dir_old,listed_dir_new):
     offset_old = 0
@@ -75,12 +106,27 @@ def print_compared_dirs(listed_dir_old,listed_dir_new):
             str_from_new_dir = ' '*STRING_LENGTH
             
         if new_elem not in listed_dir_old:
-            print(colorama.Back.GREEN,strnum,' '*STRING_LENGTH,str_from_new_dir,'\033[39m')
+            print(colorama.Back.GREEN,strnum,' '*STRING_LENGTH,str_from_new_dir,colorama.Back.BLACK)
             offset_old = offset_old + 1
         elif old_elem not in listed_dir_new:
-            print(colorama.Back.RED , strnum,str_from_old_dir,' '*STRING_LENGTH,'\033[39m')
+            print(colorama.Back.RED , strnum,str_from_old_dir,' '*STRING_LENGTH,colorama.Back.BLACK)
             offset_new = offset_new + 1
         elif old_elem == new_elem:
-            print(colorama.Back.BLACK,strnum,str_from_old_dir,str_from_new_dir ,'\033[39m')
+            try:
+                f1 = open(dir_old+old_elem,'rb').read()
+                f1h = hash(f1)
+                f1.close()
+            except:
+                pass
+            try:
+                f2 = open(dir_new+new_elem,'rb').read()
+                f2h = hash(f2)
+                f2.close()
+            except:
+                pass
+            if f1h == f2h:
+                print(colorama.Back.BLACK ,strnum,str_from_old_dir,str_from_new_dir ,'\033[39m')
+            else:
+                print(colorama.Back.YELLOW,strnum,str_from_old_dir,str_from_new_dir ,colorama.Back.BLACK)
 
 term()
